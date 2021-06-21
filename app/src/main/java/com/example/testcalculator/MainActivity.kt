e65c3eab78a78d5114bb9d48c3fd9b697e8b649d
RecklessCow <38892483+RecklessCow@users.noreply.github.com>
2021-06-21 18:57:25 +0400

    landscapelayout

--- app/src/main/java/com/example/testcalculator/MainActivity.kt
+++ app/src/main/java/com/example/testcalculator/MainActivity.kt
@@ -1,11 +1,29 @@
-package com.example.testcalculator
+    package com.example.testcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+    private const val TAG  = "mainactivity"
+
+    class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button7 = findViewById<Button>(R.id.button7)
+        val button6 = findViewById<Button>(R.id.button6)
+        val button5 = findViewById<Button>(R.id.button5)
+        val button4 = findViewById<Button>(R.id.button4)
+        val button3 = findViewById<Button>(R.id.button3)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button1 = findViewById<Button>(R.id.button1)
+
+        button7.setOnClickListener(){
+            Log.d(TAG,"VABOzYLNEM")
+        }
     }
 }
